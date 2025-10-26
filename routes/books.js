309cdeda094d0038d635bc4b539e3fd779ec61b5
Subhiksha B <subhiksha132002@gmail.com>
2025-10-26 23:27:22 +0530

    Completed CRUD operations for books

--- routes/books.js
+++ routes/books.js
@@ -3,25 +3,74 @@ import Book from "../model/bookSchema.js";
 
 const router = express.Router();
 
-router.get('/', async (req,res) => {
+router.get('/', async (req, res) => {
     try {
         const books = await Book.find();
         res.status(200).json(books);
-    } catch(error){
+    } catch (error) {
         res.status(500).send(error);
     }
 });
 
-router.post('/', async (req,res) => {
-    try{
+router.get('/:id', async (req, res) => {
+    try {
+
+        const id = req.params.id;
+        const bookExist = await Book.findOne({ _id: id });
+        if (!bookExist) {
+            return res.status(404).json({ message: "Book Not Found" });
+        }
+        const book = await Book.findById(id);
+        res.status(200).json(book);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
 
         const book = new Book(req.body.book);
         await book.save();
         res.status(201).json(book);
 
-    } catch(error) {
-         res.status(500).send(error);
+    } catch (error) {
+        res.status(500).send(error);
     }
 });
+
+
+
+router.put('/:id', async (req, res) => {
+    try {
+
+        const id = req.params.id;
+        const bookExist = await Book.findOne({ _id: id });
+        if (!bookExist) {
+            return res.status(404).json({ message: "Book Not Found" });
+        }
+
+        const updatedBook = await Book.findByIdAndUpdate(id, req.body.book, { new: true });
+        res.status(200).json(updatedBook);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const bookExist = await Book.findOne({ _id: id });
+        if (!bookExist) {
+            return res.status(404).json({ message: "Book Not Found" });
+        }
+
+        await Book.findByIdAndDelete(id);
+        res.status(204).json({ message: "Book deleted successfully" });
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
 
 export default router;
